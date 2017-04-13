64589f5d7292e0e4e4f8c8cfc59ef2c43ab43ebe
Ted Yapo <ted.yapo@zednaughtlabs.com>
2017-04-13 16:17:40 -0400

    cleaned code; exposed explicit parameters

--- make_cf32_IQ_file.py
+++ make_cf32_IQ_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 #
-# make_cf32_IQ_file.py - generate a complex float (cf32) file with
+# make_cf32_IQ_file.py - generate a complex float IQ (cf32) file with
 #  all US allocated MW broadcasting channels announcing their frequency
 #  in a synthesized voice.  Output can be read/decoded by gqrx SDR software 
 #
-#  requires thta 'flite' speech synthesizer be installed,
+#  requires that 'flite' speech synthesizer be installed,
 #     and curently uses the cmu_us_ljm.flitevox voice which must be downloaded
 #
 
@@ -16,12 +16,33 @@ import tempfile
 import math
 import sys
 
+f_speech = 16000 # hard-coded inside speech synthesizer
+upsample_rate = 74
+f_sample = f_speech * upsample_rate
+#f_local_oscillator = 1.6e6
+f_local_oscillator = 1.115e6
+amplitude_scale = 1.679 # empirically determined to give +/- 1 output
 
-# loop over 530 - 1700  @ 10 kHz spacing (118 channels)
+# target frequencies in kHz
+f_min = 530
+f_max = 1700
+f_step = 10
+
+# quantize to 8-bits for testing, or leave in 32-bit float precision
+#  (output in 32-bit float format either way)
+quantization_test = False
+
+sys.stderr.write('f_sample = ' + str(f_sample) + '\n');
+sys.stderr.write('f_local_oscillator = ' + str(f_local_oscillator) + '\n');
+sys.stderr.write('bandwidth = ' + str(f_sample) + '\n');
+
+
+# loop over frequencies
 #   and create wav files of synthesized speech frequency announcements 
 wav_files = {}
 frequencies = []
-for freq in range(530, 1705, 10):
+for freq in range(f_min, f_max + f_step/2, f_step):
+  sys.stderr.write('generating voice for ' + str(freq) + ' kHz\r') 
   frequencies.append(freq)
 
   # convert frequency to sequence of (phoentic) numerals
@@ -45,12 +66,12 @@ for freq in range(530, 1705, 10):
                                   wav_file.name])
   wav_file.seek(0)
 
+sys.stderr.write('\n') 
+
 # find longest speech clip
 wavs = {}
-amplitudes = {}
 max_frames = 0
 for freq in frequencies:  
-  amplitudes[freq] = 0.
   # use wave to parse wav data
   wavs[freq] = wave.open(wav_files[freq], 'r')
   #print wavs[freq].getparams()
@@ -60,28 +81,44 @@ for freq in frequencies:
 
 
 # modulate and combine signals
-# hard-coded to upsample 16kHz speech samples by 200x to obtain
-#   3.2MHz RF samples
 t = 0.
-dt = 1./3.2e6
-wl = 2. * math.pi * 1.6e6
+wl = 2. * math.pi * f_local_oscillator
 scale = 1./len(frequencies)
+amplitudes = {}
+sample_idx = 0.
+max_amplitude = 0.
 for frame in range(0, max_frames):
+  sys.stderr.write('generating frame ' + 
+                   str(frame) + '/' + str(max_frames) + '\r')
   for freq in frequencies:
     if frame >= wavs[freq].getnframes():
-      amplitudes[freq] = 0.
+      amplitudes[freq] = -0.
     else:
       amplitudes[freq] = struct.unpack('<h',
                                        wavs[freq].readframes(1))[0] / 32768.
 
-  for upsample in range(0, 200):
-    t += dt
+  for upsample in range(0, upsample_rate):
+    t = sample_idx / f_sample
+    sample_idx += 1.
     I = 0.
     Q = 0.
     for freq in frequencies:
       wc = 2. * math.pi * freq * 1.e3
       I += scale * (0.5 + 0.5 * amplitudes[freq]) * math.cos(t * (wc - wl))
       Q += scale * (0.5 + 0.5 * amplitudes[freq]) * math.sin(t * (wc - wl))
+    I *= amplitude_scale
+    Q *= amplitude_scale
+    if abs(I) > max_amplitude:
+      max_amplitude = abs(I)
+    if abs(Q) > max_amplitude:
+      max_amplitude = abs(Q)
+    I = min(1., max(I, -1.))
+    Q = min(1., max(Q, -1.))
+    if quantization_test:
+      I = round(I * 127.) / 128.;
+      Q = round(Q * 127.) / 128.;
     cf32_iq = struct.pack('ff', I, Q)
     sys.stdout.write(cf32_iq)
-  
+
+sys.stderr.write('\n')  
+sys.stderr.write('max amplitude = ' + str(max_amplitude) + '\n')  
